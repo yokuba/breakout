07b5fc9db1f1ca4c90f38338e25cbcc9c4454147
Alicia MacLennan <aflyingfemme@gmail.com>
2016-03-18 13:18:13 -0400

    Add js file

---
+++ game.js
@@ -0,0 +1,17 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
