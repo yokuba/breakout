0225fccbf8ae5bb07178358979f08995e8ea271b
Alicia MacLennan <aflyingfemme@gmail.com>
2016-03-18 14:38:37 -0400

    Add ball movement

--- game.js
+++ game.js
@@ -1,17 +1,29 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = -2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function drawBall(){
+  ctx.beginPath();
+  ctx.arc( x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+  if (x < 0){
+    x = Math.abs(x);
+  }
+  if (y < 0){
+    y = Math.abs(y);
+  }
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
+}
+setInterval(draw, 25);
